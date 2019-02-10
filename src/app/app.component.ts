174a4aa177fd2f3f8eb299d21500b100282b75b9
PeTerSunz <nattachai.cha@ku.th>
2019-02-10 14:35:09 +0700

    'test'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // cards: Card[] = [];
+     cards: Card[] = new Array;
+
+     get sorted(): Card[]{
+       console.log('access');
+       return this.cards.sort((a, b) => b.votes - a.votes); // arrow function คือฟังชันในฟังชัน
+     }
+    onclick(firstname: HTMLInputElement,lastname: HTMLInputElement){
+      console.log(firstname.value,lastname.value);
+      const card = new Card(firstname.value, lastname.value,0);
+      this.cards.push(card); //การที่เราจะยัดค่าเข้าไปใน array ต่อท้าย
+      console.log(this.cards);
+      firstname.value = '';
+      lastname.value = '';
+    }
 }
